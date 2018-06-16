6830720cb5ceccca79a078351e3cb6e08468119e
JocelynTse <jocelyn.s.tse@gmail.com>
2018-06-15 19:32:17 -0500

    Added moment.js & time calculation lines

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,20 +16,60 @@
   let frequency;
   let nextArrival;
   let minutesAway;
+  let freqNew;
+  let firstTime;
+  let timeNew;
 
   $("#submit").on("click", function(event)  {
     event.preventDefault();
 
-    let nameNew = $("#trainName").val().trim();
-    let destNew = $("#destination").val().trim();
-    let timeNew = $("#firstTrain").val().trim();
-    let freqNew = $("#frequency").val().trim();
+    var nameNew = $("#trainName").val().trim();
+    var destNew = $("#destination").val().trim();
+    timeNew = $("#firstTrain").val().trim();
+    freqNew = $("#frequency").val().trim();
 
     database.ref().push({
-      name = nameNew,
-      destination = destNew,
-      frequency = freqNew,
-      nextArrival = ,
-      mintuesAway = ,
+      name: nameNew,
+      destination: destNew,
+      frequency: freqNew,
+      //nextArrival:,
+      //minutesAway:,
     })
+  })
+
+  database.ref().on("child_added", function(snapshot) {
+    console.log(snapshot.val());
+
+    frequency = freqNew;
+
+    firstTime = timeNew;
+
+    let timeConverted = moment(firstTime, "HH:mm"). subtract(1, "years");
+    console.log(timeConverted);
+
+    let currentTime = moment();
+    console.log("Current Time: " + moment(currentTime).format("hh.mm"));
+
+    let diffTime = moment().diff(moment(timeConverted), "minutes");
+    console.log("Difference: " + diffTime);
+
+    let tRemainder = diffTime % frequency;
+    console.log(tRemainder);
+
+    let tMinsTrain = frequency - tRemainder;
+    console.log("Minutes Next Train: " + tMinsTrain);
+
+    let nextTrain = moment().add(tMinsTrain, "minutes");
+    console.log("Arrival: " + moment(nextTrain).format("hh.mm"));
+
+    name = snapshot.val("name");
+    destination = snapshot.val("destination");
+    frequency = snapshot.val("frequency");
+    nextArrival = 
+
+
+    let tRow = $("<tr>");
+    
+    let nameTab = $("<td").text() 
+    
   })
